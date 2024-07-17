ac5bc6e9ae9f424d1e41e932d87a5a7887df83b5
Arkadiusz Sarach <ajek1993@gmail.com>
2024-07-17 13:23:22 +0200

    add keyboard draft

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,13 @@ function App() {
       </div>
       <HangmanDrawing />
       <HangmanWord />
-      <Keyboard />
+      <div
+        style={{
+          alignSelf: "stretch",
+        }}
+      >
+        <Keyboard />
+      </div>
     </div>
   );
 }
